91c5be0c2166f333e694bf1eb00dd48b03ca04db
Guillaume J. Charmes <guillaume@charmes.net>
2014-07-17 13:28:24 -0400

    Add MakeRaw test

--- raw/raw_test.go
+++ raw/raw_test.go
@@ -3,6 +3,9 @@ package raw
 import (
 	"syscall"
 	"testing"
+	"time"
+
+	"github.com/kr/pty"
 )
 
 func TestCfMakeRaw(t *testing.T) {
@@ -39,6 +42,65 @@ func TestCfMakeRaw(t *testing.T) {
 	}
 	if got := *termios; want != got {
 		t.Fatalf("Unexpected Raw termios.\nGot:\t %#v\nWant:\t %#v\n", got, want)
+	}
+}
+
+func TestMakeRaw(t *testing.T) {
+	// Create a PTY pair to play with
+	master, slave, err := pty.Open()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer master.Close()
+	defer slave.Close()
+
+	// Apply the raw mode on the slave
+	slaveTermios, err := MakeRaw(slave.Fd())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Make a copy of the original termios and manually apply cfmakeraw
+	slaveTermiosOrig := *slaveTermios
+	CfMakeRaw(&slaveTermiosOrig)
+
+	// Retrieve the new termios on the slave after NakeRaw
+	slaveTermiosRaw, err := TcGetAttr(slave.Fd())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Make sure the new termios are the one we want
+	if slaveTermiosOrig != *slaveTermiosRaw {
+		t.Fatalf("Unepexpected termios.\nGot:\t %#v\nWant:\t %#v\n", *slaveTermiosRaw, slaveTermiosOrig)
+	}
+
+	// Simple read/write test on the master/slave pair
+	want := "hello world!"
+	go master.WriteString(want)
+
+	var (
+		buf = make([]byte, 64)
+		c   = make(chan struct{})
+	)
+
+	// Without raw mode, as there is no \n, read will block forever
+	go func() {
+		defer close(c)
+		n, err := slave.Read(buf)
+		if err != nil {
+			t.Fatal(err)
+		}
+		buf = buf[:n]
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		buf = []byte("timeout")
+		close(c)
+	}()
+	<-c
+	if got := string(buf); got != want {
+		t.Fatalf("Unexpected result.\nGot: %s\nWant: %s\n", got, want)
 	}
 }
 
