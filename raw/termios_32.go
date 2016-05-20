f68910e5216a82b3ff5679947e8df52c4cd2f8ab
Michael Neumann <mneumann@ntecs.de>
2016-05-20 12:28:27 +0200

    Fix build for DragonFly BSD

--- raw/termios_32.go
+++ raw/termios_32.go
@@ -1,11 +1,11 @@
-// +build linux freebsd
+// +build linux freebsd dragonfly
 
 package raw
 
 // Termios holds the TTY attributes. See man termios(4).
 // Tested on linux386, linux/arm, linux/amd64,
 //           freebsd/386, freebsd/arm, freebsd/amd64.
-// See tremios_64.go for darwin.
+// See termios_64.go for darwin.
 type Termios struct {
 	Iflag  uint32
 	Oflag  uint32
